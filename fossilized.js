5717bf0923c380dbddb02be13d23254497547538
Melissa I <phoenixemere@gmail.com>
2014-02-06 01:05:33 -0500

    Two-dimensional array added.

--- fossilized.js
+++ fossilized.js
@@ -1,10 +1,82 @@
-function changeDisplay(playerChoice){
-	var tDisplay=document.getElementById('textDisplay');
-	var playerChoice;
-	if (playerChoice === 1){
-	tDisplay.value = "You are in the Salt Desert, with nothing but white sand in all directions.";
+//Global vars
+locations = [];
+//dir = [];
+map = [];
+inventory = [];
+
+function commands(buttondir){
+	tInput=document.getElementById('input').toLowerCase();
+	if (buttondir === 'noDir'){
+		buttondir === tInput;
+	}
+		navigate(locations[currentLocation], buttondir);
+}
+
+function navigate(currentLocation, direction){
+	tDisplay=document.getElementById('textDisplay');
+
+	 
+	//Location List
+	locations[0] = "You are in the Salt Desert, with nothing but white sand in all directions. The sun is beating down from above.";
+	locations[1] = "An hour's hike has led you no closer to civilization. The Salt Desert stretches in all directions...";
+	locations[2] = "An hour's hike shows no difference in the horizon, but as you go, the ground becomes more and more uneven.";
+	locations[3] = "A fifteen-minute trek leads you, unceremoniously, to an impassable barbed-wire fence, too high to climb and virtually" + 
+							" impenetrable. A metal sign on the fence reads 'TYLAVIAN BORDER' in three different languages, only one of which" +
+							" you are able to read.";
+	locations[4] = "THE STONE OASIS: That's what the sign says, anyway. This small oasis features a ring of several"  +  
+							" small huts constructed entirely of gray stone, all adorned with brightly colored awnings that" + 
+							" provide shade. A stone fountain is located in the center of this ring. It runs sluggishly, but hey, it's water. Take some?";				
+	locations[5] = "SALT CLIFF: You are at the top of a sheer white cliff. It's a long way down, but it does give a good view of the Carbonic Valley," +
+							" an expansive of dark gray sand that stretches as far as the eye can see. There is sparse human activity, but a cluster of tents is" +
+							" clearly visible from up here. A southerly path seems to lead down the cliff.";
+	locations[6] = "The path is rocky and treacherous. There is a small opening in the cliff, leading into some sort of dark cave.";
+	locations[7] = "You are getting close to heat exhaustion. In the distance, there's something on the horizon, but it could be just a mirage.";
+	locations[8] = "Within the cave, you find one Aqua Gem! Congratulations! You've won this game.";
+	//mention aqua gem earlier when water taken
+	
+	//Direction list
+	
+	switch (direction){
+	case "n":
+		dir = 0;
+		break;
+	case "e":
+		dir = 1;
+		break;
+	case "s":
+		dir = 2;
+		break;
+	case "w":
+		dir = 3;
+		break;
+	default:
+		dir = 4;
+		break;
+	}
+	
+	//Mapping
+	//Two-dimensional array outputs next location based on direction, of the form:
+	//map array order = locations array order, e.g. map[0] = cases for locations[0].
+	//cases for [n, e, s, w]
+	map = [
+	[1, 2, 3, 4],
+	[-1, -1, 0, 7],
+	[-1, 5, -1, 0],
+	[0, -1, -1, -1],
+	[7, 1, -1, -1],
+	[-1, -1, 6, 2],
+	[5, -1, -1, 8],
+	[-1, 1, 4, -1],
+	[-1, 6, -1, -1]];
+	
+	currentLocation = locations[map[0][dir]];
+	tDisplay.value = currentLocation;
+	
+
+	/*if (currentLocation === 1 && direction === 0){
+	tDisplay.value = currentLocation;
 	}
 	else{
-	tDisplay.value = "playerChoice was invalid.";
-	}
+	tDisplay.value = tDisplay.value;
+	}*/
 }
